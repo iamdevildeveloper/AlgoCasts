4b05703a132b1e739e3f60fcdb79d64af005d833
DevilDeveloper <iamdevildeveloper@gmail.com>
2024-09-20 20:26:19 +0530

    Resolved palindrome test

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,24 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    var explodedString = str.split('');
+    var explodedStringLength = explodedString.length;
+    var halfValue = Math.floor(explodedStringLength / 2);
+    var firstCharacters = str.substring(0,halfValue);
+    var minus = (explodedStringLength - halfValue*2);
+    var subStringLength = halfValue+1;
+    if(minus == 0){
+        subStringLength = halfValue;
+    }
+    var lastCharacters =  str.substring(subStringLength, explodedStringLength);
+    var reversedString = '';
+    for(character of lastCharacters){
+        reversedString = character + reversedString;
+    }
+    lastCharacters = reversedString;
+    if(firstCharacters == lastCharacters) return true;
+    return false;
+}
 
 module.exports = palindrome;
